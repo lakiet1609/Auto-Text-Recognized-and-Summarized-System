48c100ada74f6bf2a79dea83dcd7dc42c491286a
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-10-17 16:00:00 +0700

    building project structure

---
+++ structure.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+list_files = [
+    'src/components/__init__.py',
+    'src/utils/__init__.py',
+    'triton_models/.gitkeep'
+]
+
+for file in list_files:
+    file = Path(file)
+    file_dir, file_name = os.path.split(file)
+
+    if file_dir != '':
+        os.makedirs(file_dir, exist_ok=True)
+
+    if (not os.path.exists(file)) or (os.path.getsize(file) == 0):
+        with open(file, 'w') as f:
+            pass
