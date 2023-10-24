12d7e99d1275478be3c0ce0a53208eca356d617f
lakiet1609 <kiettuanminhla1609@gmail.com>
2023-10-24 23:59:54 +0700

    update ensemble config for text recognition

--- src/components/predict_rec.py
+++ src/components/predict_rec.py
@@ -1,23 +1,17 @@
 import os
 import sys
-from PIL import Image
+
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(__dir__)
 sys.path.insert(0, os.path.abspath(os.path.join(__dir__, '../..')))
 
-os.environ["FLAGS_allocator_strategy"] = 'auto_growth'
 
-import cv2
-import numpy as np
-import paddle
-from src.common.ppocr.postprocess import build_post_process
 import tritonclient.grpc as grpcclient
 
 
 class TextRecognizer(object):
-    def __init__(self, args):
+    def __init__(self):
         self.dictionary = (open('common/ppocr/utils/en_dict.txt', 'r').read().split('\n'))
-
         self.url = '192.168.1.10:8001'
         self.triton_client = grpcclient.InferenceServerClient(url=self.url, verbose=False)
         self.model_name = 'text_rec'
@@ -33,40 +27,25 @@ class TextRecognizer(object):
     
 
     def __call__(self, dt_boxes, ori_img):
-        dt_boxes_shape = list(dt_boxes.shape)
-        images_shape = list(ori_img.shape)
         inputs = []
         outputs = []
         
-        inputs.append(grpcclient.InferInput("dt_boxes", dt_boxes_shape, "FP32"))
-        inputs.append(grpcclient.InferInput("images", images_shape, "UINT8"))
+        inputs.append(grpcclient.InferInput("dt_boxes", list(dt_boxes.shape), "FP32"))
+        inputs.append(grpcclient.InferInput("images", list(ori_img.shape), "UINT8"))
         inputs[0].set_data_from_numpy(dt_boxes)
         inputs[1].set_data_from_numpy(ori_img)
-
-        outputs.append(grpcclient.InferRequestedOutput("infer_text_rec_output"))
-
-        results = self.triton_client.infer(model_name=self.model_name, inputs=inputs, outputs=outputs)
-        
-        infer_text_rec_output = results.as_numpy("infer_text_rec_output")
-        
-        infer_text_rec_output_shape = list(infer_text_rec_output.shape)
-        inputs = []
-        outputs = []
-        
-        inputs.append(grpcclient.InferInput("post_rec_input", infer_text_rec_output_shape, "FP32"))
-        inputs[0].set_data_from_numpy(infer_text_rec_output)
 
         outputs.append(grpcclient.InferRequestedOutput("post_rec_output"))
         outputs.append(grpcclient.InferRequestedOutput("post_rec_output_score"))
 
-        results = self.triton_client.infer(model_name='post_text_rec', inputs=inputs, outputs=outputs)
+        results = self.triton_client.infer(model_name=self.model_name, inputs=inputs, outputs=outputs)
         
         texts = results.as_numpy("post_rec_output")
         scores = results.as_numpy("post_rec_output_score")
         
-        for text in texts:
+        for text, score in zip(texts,scores):
             ori_text = self.decode(text)
-            print(ori_text)
+            print(ori_text, score)
 
         return texts, scores
 
--- src/components/predict_system.py
+++ src/components/predict_system.py
@@ -1,15 +1,11 @@
 import os
 import sys
-import subprocess
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(__dir__)
 sys.path.insert(0, os.path.abspath(os.path.join(__dir__, '../..')))
 
-os.environ["FLAGS_allocator_strategy"] = 'auto_growth'
-
 import cv2
-import copy
 import numpy as np
 import src.common.utility as utility
 import src.components.predict_rec as predict_rec
@@ -19,7 +15,7 @@ import src.components.predict_det as predict_det
 class TextSystem(object):
     def __init__(self, args):
         self.text_detector = predict_det.TextDetector()
-        self.text_recognizer = predict_rec.TextRecognizer(args)
+        self.text_recognizer = predict_rec.TextRecognizer()
         self.args = args
     
     def __call__(self, img):
@@ -27,7 +23,6 @@ class TextSystem(object):
             return None
         
         dt_boxes = self.text_detector(img)
-
         text, score = self.text_recognizer(dt_boxes, img)
 
 def main(args):
